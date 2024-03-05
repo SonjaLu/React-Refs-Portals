d51f20cd152e7166ae38184850576c2dcda29c66
SonjaLu <s_lueg@yahoo.com>
2024-03-05 22:09:22 +0100

    adding divs and css

--- src/components/Player.jsx
+++ src/components/Player.jsx
@@ -1,6 +1,7 @@
-import { useState } from "react";
+import { useState, useRef } from "react";
 
 export default function Player() {
+  const playerName = useRef();
 
 const [enteredPlayerName, setEnteredPlayerName] = useState('');
 const [submitted, setSubmitted] = useState(false);
@@ -10,15 +11,15 @@ setEnteredPlayerName(event.target.value);
 }
 
 function handleClick() {
-  setSubmitted(true);
-
+  setEnteredPlayerName(playerName.current.value);
+playerName.current.value = '';
 }
 
   return (
     <section id="player">
-      <h2>Welcome {submitted ? enteredPlayerName : 'unknown entity'}</h2>
+      <h2>Welcome {enteredPlayerName ?? 'unknown entity'}</h2>
       <p>
-        <input type="text" onChange={handleChange} value={enteredPlayerName}/>
+        <input ref={playerName} type="text" onChange={handleChange} value={enteredPlayerName}/>
         <button onClick={handleClick}>Set Name</button>
       </p>
     </section>
