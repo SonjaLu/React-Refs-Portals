4cbb76bf5a8d7c638c3eeca9733e01ead6c712af
SonjaLu <s_lueg@yahoo.com>
2024-03-06 22:07:59 +0100

    set timer and popup

--- src/components/TimerChallenge.jsx
+++ src/components/TimerChallenge.jsx
@@ -1,16 +1,42 @@
+import { useState, useRef } from 'react';
+import ResultModal from './ResultModal.jsx';
+
+
 export default function TimerChallenge({ title, targetTime }) {
+   const timer = useRef();
+
+    const [timerStarted, setTimerStarted] = useState(false);
+const [timerExpired, setTimerExpired] = useState(false);
+
+
+
+function handleStart() {
+   setTimerStarted(true);
+   timer.current = setTimeout(() => {
+        setTimerExpired(true);
+    }, targetTime * 1000);
+}
+
+function handleStop() {
+clearTimeout(timer.current);
+}
     return (
+        <>
+       {timerExpired && <ResultModal targetTime={targetTime} result="lost"/>}
     <section className="challenge">
     <h2>{title}</h2>
+    
     <p className="challenge-time">
 {targetTime} second{targetTime > 1 ? 's' : ''}
     </p>
     <p>
-        <button>Start</button>
+        <button onClick={timerStarted ? handleStop : handleStart}>
+            {timerStarted ? 'Stop' : 'Start'}</button>
     </p>
-    <p className="">
-        Time is running... / Timer inactive
+    <p className={timerStarted ? 'active' : undefined}>
+       {timerStarted ? 'Time is running...' :  'Timer inactive'}
     </p>
     </section>
+    </>
     );
 }
